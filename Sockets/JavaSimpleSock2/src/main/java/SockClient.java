3fe5e2b18776ae4b48c783ff5b2f29f9055c00de
pryce.holmes <pryce.holmes0@gmail.com>
2024-10-17 15:41:19 -0500

    add aws ip address

--- Sockets/JavaSimpleSock2/src/main/java/SockClient.java
+++ Sockets/JavaSimpleSock2/src/main/java/SockClient.java
@@ -14,7 +14,7 @@ import java.io.*;
 class SockClient {
   public static void main (String args[]) {
     Socket sock = null;
-    String host = "localhost";
+    String host = "18.116.33.25";
     String message = "HI";
     Integer number = 100;
 
